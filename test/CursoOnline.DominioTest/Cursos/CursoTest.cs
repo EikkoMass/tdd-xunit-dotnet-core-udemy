d052b570ce263d5597197176f227543b36ca54a9
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-01-09 16:16:02 -0300

    Setup e Cleanup

--- test/CursoOnline.DominioTest/Cursos/CursoTest.cs
+++ test/CursoOnline.DominioTest/Cursos/CursoTest.cs
@@ -1,11 +1,35 @@
 using CursoOnline.DominioTest._Util;
 using ExpectedObjects;
 using Xunit;
+using Xunit.Abstractions;
 
 namespace CursoOnline.DominioTest.Cursos;
 
-public class CursoTest
+public class CursoTest : IDisposable
 {
+
+    private readonly ITestOutputHelper _output;
+    
+    private readonly string _nome;
+    private readonly double _cargaHoraria;
+    private readonly double _valor;
+    private readonly PublicoAlvo _publicoAlvo;
+    
+    public CursoTest(ITestOutputHelper output)
+    {
+        _output = output;
+
+        _nome = "Informatica Basica";
+        _cargaHoraria = 80;
+        _valor = 950;
+        _publicoAlvo = PublicoAlvo.Estudante;
+    }
+    
+    public void Dispose()
+    {
+        _output.WriteLine("Dispose sendo executado");
+    }
+    
     [Fact]
     public void DeveCriarCurso()
     {
@@ -30,15 +54,8 @@ public class CursoTest
     [InlineData(null)] // cada inlineData corresponde a 1 execucao com a variacao sendo o valor adicionado no mesmo
     public void NaoDeveCursoTerUmNomeInvalido(string nomeInvalido)
     {
-        var cursoEsperado = new
-        {
-            Nome = "John Doe",
-            PublicoAlvo = PublicoAlvo.Estudante,
-            Valor = (double) 950,
-            CargaHoraria = (double) 80
-        };
         
-        Assert.Throws<ArgumentException>(() => new Curso(nomeInvalido, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, cursoEsperado.Valor))
+        Assert.Throws<ArgumentException>(() => new Curso(nomeInvalido, _cargaHoraria, _publicoAlvo, _valor))
             .ComMensagem("Nome Invalido");
     }
 
@@ -49,15 +66,7 @@ public class CursoTest
 
     public void NaoDeveCursoTerUmaCargaHorariaMenorQueUm(double cargaHorariaInvalida)
     {
-        var cursoEsperado = new
-        {
-            Nome = "John Doe",
-            PublicoAlvo = PublicoAlvo.Estudante,
-            Valor = (double) 950,
-            CargaHoraria = (double) 999
-        };
-        
-        Assert.Throws<ArgumentException>(() => new Curso(cursoEsperado.Nome, cargaHorariaInvalida, cursoEsperado.PublicoAlvo, cursoEsperado.Valor))
+        Assert.Throws<ArgumentException>(() => new Curso(_nome, cargaHorariaInvalida, _publicoAlvo, _valor))
             .ComMensagem("Carga Horaria Invalida"); 
     }
     
@@ -68,16 +77,8 @@ public class CursoTest
 
     public void NaoDeveCursoTerUmValorMenorQueUm(double valorInvalido)
     {
-        var cursoEsperado = new
-        {
-            Nome = "John Doe",
-            PublicoAlvo = PublicoAlvo.Estudante,
-            Valor = (double) 950,
-            CargaHoraria = (double) 999
-        };
-
         Assert.Throws<ArgumentException>(() =>
-                new Curso(cursoEsperado.Nome, cursoEsperado.CargaHoraria, cursoEsperado.PublicoAlvo, valorInvalido))
+                new Curso(_nome, _cargaHoraria, _publicoAlvo, valorInvalido))
             .ComMensagem("Valor Invalido");
     }
 }
