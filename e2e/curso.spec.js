89adb6a581526a784fc0349acfb9992a54aceb02
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-01-14 20:36:00 -0300

    Trabalhando com mais campos de formulário

--- e2e/curso.spec.js
+++ e2e/curso.spec.js
@@ -4,10 +4,12 @@ fixture('Curso')
   .page('https://localhost:3000/Curso/Novo')
 
   test('Deve criar um novo curso', async t => {
-      await t.typeText(Selector("[name='Nome']"), 'Curso TestCafe2')
+      await t.typeText(Selector("[name='Nome']"), 'Curso TestCafe4')
       await t.typeText(Selector("[name='Descricao']"), 'teste')
       await t.typeText(Selector("[name='CargaHoraria']"), '10')
-      await t.typeText(Selector("[name='PublicoAlvo']"), 'Empregado')
+      await t.click(Selector("[name='PublicoAlvo']"))
+      await t.click(Selector("option[value='Empregado']"))
+      await t.click(Selector("[value='Online']"))
       await t.typeText(Selector("[name='Valor']"), '1000')
 
       await t.click(Selector(".btn-success"))
