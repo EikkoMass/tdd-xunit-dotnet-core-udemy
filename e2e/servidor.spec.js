c19e27f048d48f8d6286fa8440929882dc50415d
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-01-14 17:28:29 -0300

    TestCafe - Primeiro Teste

---
+++ e2e/servidor.spec.js
@@ -0,0 +1,8 @@
+import { Selector } from 'testcafe';
+
+fixture('Servidor')
+  .page('localhost:3000')
+
+test('Validando se esta de pe', async t => {
+  await t.expect(Selector('title').innerText).eql('Home Page - CursoOnline.Web')
+})
