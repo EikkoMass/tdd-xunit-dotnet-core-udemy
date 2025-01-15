2d196e106c38de783ed27029f80b6503a83bccc1
Eric Henrique Massaneiro <ericmassaneiro@gmail.com>
2025-01-14 21:00:15 -0300

    Validando campos obrigatórios / repetidos

--- e2e/curso.spec.js
+++ e2e/curso.spec.js
@@ -4,7 +4,7 @@ fixture('Curso')
   .page('https://localhost:3000/Curso/Novo')
 
   test('Deve criar um novo curso', async t => {
-      await t.typeText(Selector("[name='Nome']"), 'Curso TestCafe4')
+      await t.typeText(Selector("[name='Nome']"), 'Curso TestCafe ' + (new Date()).toString());
       await t.typeText(Selector("[name='Descricao']"), 'teste')
       await t.typeText(Selector("[name='CargaHoraria']"), '10')
       await t.click(Selector("[name='PublicoAlvo']"))
@@ -16,3 +16,11 @@ fixture('Curso')
       
       await t.expect(Selector("title").innerText).eql('Listagem de cursos - CursoOnline.Web')
   })
+
+test('Deve evitar registrar curso com nome repetido', async t => {
+    await t.typeText(Selector("[name='Nome']"), 'Curso TestCafe');
+    await t.typeText(Selector("[name='Descricao']"), 'teste')
+    await t.click(Selector(".btn-success"))
+
+    await t.expect(Selector(".toast-message").withText('Nome do curso ja consta no banco de dados').count).eql(1);
+})
